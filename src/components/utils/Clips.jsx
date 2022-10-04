5c63d7472cf8a8e085e2180ab4ed820840a728b0
Roman Trynko <trynkoroman25@gmail.com>
2022-10-04 12:34:40 +0300

    play icon added

--- src/components/utils/Clips.jsx
+++ src/components/utils/Clips.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 const Clips = ({ clip, imgsrc }) => {
   return (
-    <div className='relative h-28 w-32 rounded-xl overflow-hidden group cursor-pointer transition-all duration-300 lg:w-28 md:w-24 sm:w-16 lg:h-24 md:h-20 sm:h-14'>
+    <div className='clip-container group'>
       <img
         src={imgsrc}
         alt='img/clips'
         className='inset-0 flex h-full w-full object-cover absolute top-0 left-0 right-0 rounded-xl opacity-100 z-10 transition-opacity duration-500'
       />
-      <div>PlayIcon</div>
+      <div className='play-icon-container'>
+        <PlayIcon className='icon-style' />
+      </div>
       <video
         autoPlay={true}
         loop={true}
