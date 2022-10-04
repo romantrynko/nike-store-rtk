c0a6c5115c0ccc00cfa7fe5153c47ffe32bf54a2
Roman Trynko <trynkoroman25@gmail.com>
2022-10-04 13:00:58 +0300

    social icons added

--- src/components/utils/Clips.jsx
+++ src/components/utils/Clips.jsx
@@ -7,7 +7,7 @@ const Clips = ({ clip, imgsrc }) => {
       <img
         src={imgsrc}
         alt='img/clips'
-        className='inset-0 flex h-full w-full object-cover absolute top-0 left-0 right-0 rounded-xl opacity-100 z-10 transition-opacity duration-500'
+        className='video-banner'
       />
       <div className='play-icon-container'>
         <PlayIcon className='icon-style' />
@@ -17,7 +17,7 @@ const Clips = ({ clip, imgsrc }) => {
         loop={true}
         muted={true}
         playsInline={true}
-        className='absolute top-0 left-0 right-0 flex h-full w-full object-cover opacity-0 z-0 group-hover:opacity-100 group-hover:z-50 rounded-xl'>
+        className='video'>
         <source
           type='video/mp4'
           src={clip}
