e8193b06b0131c492962a578f4b771ebcc339b19
Roman Trynko <trynkoroman25@gmail.com>
2022-10-05 17:29:43 +0300

    string truncate

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Title from './utils/Title';
+import { truncate } from 'lodash';
 
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
@@ -23,57 +24,65 @@ const Stories = ({ stories: { title, news } }) => {
       425: { perPage: 1 }
     }
   };
+
   return (
     <div className='nike-container mb-11 '>
       <Title title={title} />
-      <div className=''>
+      <div className='mt-7'>
         <Splide options={splideOptions}>
-          {news.map((item, index) => (
-            <SplideSlide
-              key={index}
-              className='mb-0.5'>
-              <div className='relative grid items-center gap-4 pb-2 rounded-lg shadow shadow-slade-200 ring-1 ring-slate-200'>
-                <div className='flex items-center justify-center'>
-                  <img
-                    src={item.img}
-                    alt='img'
-                    className='w-full h-auto object-cover shadow-md shadow-slate-200 rounded-tl-lg rounded-tr-lg'
-                  />
-                </div>
-                <div className='flex items-center justify-between w-full px-4'>
-                  <div className='flex items-center gap-0.5'>
-                    <HeartIcon className='icon-style text-red-500 w-5 h-5' />
-                    <span className='text-xs font-bold'>{item.like}</span>
+          {news.map((item, index) => {
+            const truncText = truncate(item.text, {
+              length: '70'
+            });
+            return (
+              <SplideSlide
+                key={index}
+                className='mb-0.5'>
+                <div className='relative grid items-center gap-4 pb-2 rounded-lg shadow shadow-slade-200 ring-1 ring-slate-200'>
+                  <div className='flex items-center justify-center'>
+                    <img
+                      src={item.img}
+                      alt='img'
+                      className='w-full h-auto object-cover shadow-md shadow-slate-200 rounded-tl-lg rounded-tr-lg'
+                    />
                   </div>
-                  <div className='flex items-center gap-0.5'>
-                    <ClockIcon className='icon-style w-4 h-4 text-black' />
-                    <span className='text-xs font-bold'>{item.time}</span>
+                  <div className='flex items-center justify-between w-full px-4'>
+                    <div className='flex items-center gap-0.5'>
+                      <HeartIcon className='icon-style text-red-500 w-5 h-5' />
+                      <span className='text-xs font-bold'>{item.like}</span>
+                    </div>
+                    <div className='flex items-center gap-0.5'>
+                      <ClockIcon className='icon-style w-4 h-4 text-black' />
+                      <span className='text-xs font-bold'>{item.time}</span>
+                    </div>
+                    <div className='flex items-center gap-0.5'>
+                      <HashtagIcon className='icon-style text-blue-600' />
+                      <span className='text-xs font-bold text-blue-600'>
+                        {item.by}
+                      </span>
+                    </div>
                   </div>
-                  <div className='flex items-center gap-0.5'>
-                    <HashtagIcon className='icon-style text-blue-600' />
-                    <span className='text-xs font-bold text-blue-600'>
-                      {item.by}
-                    </span>
+                  <div className='grid items-center justify-items-start px-4'>
+                    <h1 className='text-base font-semibold lg:text-sm'>
+                      {item.title}
+                    </h1>
+                    <p className='text-sm text-justify lg:text-xs'>
+                      {truncText}
+                    </p>
+                  </div>
+                  <div className='flex items-center justify-center px-4 w-full'>
+                    <a
+                      href={item.url}
+                      target='_blank'
+                      role={'button'}
+                      className='w-full bg-slade-900 bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5 button-theme'>
+                      {item.btn}
+                    </a>
                   </div>
                 </div>
-                <div className='grid items-center justify-items-start px-4'>
-                  <h1 className='text-base font-semibold lg:text-sm'>
-                    {item.title}
-                  </h1>
-                  <p className='text-sm text-justify lg:text-xs'>{item.text}</p>
-                </div>
-                <div className='flex items-center justify-center px-4 w-full'>
-                  <a
-                    href={item.url}
-                    target='_blank'
-                    role={'button'}
-                    className='w-full bg-slade-900 bg-gradient-to-b from-slate-900 to-black shadow-md shadow-black text-center text-slate-100 py-1.5 button-theme'>
-                    {item.btn}
-                  </a>
-                </div>
-              </div>
-            </SplideSlide>
-          ))}
+              </SplideSlide>
+            );
+          })}
         </Splide>
       </div>
     </div>
